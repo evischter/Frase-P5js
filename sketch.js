ca65f984cbb1438da93b73980e2070f86b68600b
evischter <134930540+evischter@users.noreply.github.com>
2023-09-22 14:54:48 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    stroke("#030303")
+    background("#3BFF76");
+   fill("rgb(255,0,116)")
+    let palavra="NEVER GIVE UP"
+  text(palavra,200,200);
+    textAlign(CENTER,CENTER);
+    textSize(30);
